927a3995c1a66e4e0986b391bfd6d3e83660590f
Craig Nunemaker <craig@nunemakerllc.com>
2022-12-04 16:02:29 -0600

    made preferences public

--- Sources/GameEngine/Preferences.swift
+++ Sources/GameEngine/Preferences.swift
@@ -6,16 +6,16 @@
 //
 
 import MetalKit
-protocol MetalPreferences {
+public protocol MetalPreferences {
 	var clearColor: MTLClearColor { get }
 	var mainPixelFormat: MTLPixelFormat { get }
 	var mainDepthPixelFormat: MTLPixelFormat { get }
 }
-enum MetalClearColors{
-	static let White = MTLClearColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-	static let Green = MTLClearColor(red: 0.22, green: 0.55, blue: 0.34, alpha: 1.0)
-	static let Grey = MTLClearColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
-	static let DarkGrey = MTLClearColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1)
-	static let Black = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-	static let LimeGreen = MTLClearColor(red: 0.3, green: 0.7, blue: 0.3, alpha: 1)
+public enum MetalClearColors{
+	public static let White = MTLClearColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+	public static let Green = MTLClearColor(red: 0.22, green: 0.55, blue: 0.34, alpha: 1.0)
+	public static let Grey = MTLClearColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+	public static let DarkGrey = MTLClearColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1)
+	public static let Black = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+	public static let LimeGreen = MTLClearColor(red: 0.3, green: 0.7, blue: 0.3, alpha: 1)
 }
