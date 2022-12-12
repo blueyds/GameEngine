bf2a2fe73b16b43d0684b8cfecca4da2749cebd4
Craig Nunemaker <craig@nunemakerllc.com>
2022-12-11 18:44:48 -0600

    added a sphere mesh based on new modeliomesh

---
+++ Sources/GameEngine/SphereMesh.swift
@@ -0,0 +1,24 @@
+import MetalKit
+import ModelIO
+
+class Sphere: ModelIOMesh {
+    // the more segments the more detailed sphere will be
+    // anything less than 22 may not look right
+    
+    init(segments: UInt32 = 30){
+        super.init()
+        let allocator = MTKMeshBufferAllocator(device: ModelIOMesh.Engine!.device)
+        let mdlMesh = MDLMesh(
+            sphereWithExtent: vector_float3(1,1,1), 
+            segments: vector_uint2(repeating: segments), 
+            inwardNormals: false, 
+            geometryType: .triangles, 
+            allocator: allocator)
+        do{
+            self.mesh = try MTKMesh(mesh: mdlMesh, device: ModelIOMesh.Engine!.device)
+        } catch  {
+            print("error initializing SPhere mesh for object. Error is: \(error)")
+        }
+        assert(self.mesh?.submeshes.count == self.mesh?.vertexBuffers.count)
+    }
+}
