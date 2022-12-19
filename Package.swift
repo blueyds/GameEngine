9211ba94817e5f4cbfdd4999afbd76a5cfc7dc7f
Craig Nunemaker <craig@nunemakerllc.com>
2022-12-18 21:06:16 -0600

    error

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "GameEngine",
 	platforms: [
 		.macOS(.v10_15),
-		.iOS(.v13_4)],
+		.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library( name: "GameEngine", targets: [
