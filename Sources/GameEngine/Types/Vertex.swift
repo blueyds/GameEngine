127df2dc3d85121c552123375e4c0318b6ae6b76
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-11-13 17:53:46 -0600

    suport for vertexDescriptor

--- Sources/GameEngine/Types/Vertex.swift
+++ Sources/GameEngine/Types/Vertex.swift
@@ -14,5 +14,17 @@ public struct Vertex: sizeable {
 		self.textureCoordinate = textureCoordinate
 		self.normal = normal
 	}
+	public static weak var descriptor: VertexDescriptor{
+		VertexDescriptor(
+            attributes: [
+                VertexDescriptor.Attribute(position: 0, format: .float3, offset: 0, bufferIndex: 0), // position
+                VertexDescriptor.Attribute(position: 1, format: .float4, offset: simd_float3.size, bufferIndex: 0), // color 
+                VertexDescriptor.Attribute(position: 2, format: .float2, offset: simd_float3.size + simd_float4.size, bufferIndex: 0), // texture
+                VertexDescriptor.Attribute(position: 3, format: .float3, offset: simd_float3.size + simd_float4.size + simd_float2.size, bufferIndex: 0) // normal
+            ], 
+            layouts: [
+                VertexDescriptor.Layout(stride: Vertex.stride)
+            ])
+	}
 
 }
